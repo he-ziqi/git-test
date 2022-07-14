0b0d4d55d91c92142228aee9ee0dad0b68503e30
hzq <hzq@email.com>
2022-07-14 09:51:13 +0800

    second commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,5 +3,7 @@ package org.example;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println( "new change " );
+        System.out.println( "new change commit 2" );
     }
 }
